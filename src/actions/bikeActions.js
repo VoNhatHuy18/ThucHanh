74735faf7488b47a806ad22ca9e3aa924abad10a
VoNhatHuy18 <94149554+VoNhatHuy18@users.noreply.github.com>
2024-11-11 14:53:06 +0700

    đổi đường dẫn mock api

--- src/actions/bikeActions.js
+++ src/actions/bikeActions.js
@@ -5,7 +5,7 @@ export const FETCH_BIKES_FAILURE = 'FETCH_BIKES_FAILURE';
 export const fetchBikes = () => async (dispatch) => {
   dispatch({ type: FETCH_BIKES_REQUEST });
   try {
-    const response = await fetch('https://671db53209103098807daa1c.mockapi.io/bicycle');
+    const response = await fetch('https://673151eb7aaf2a9aff105a1b.mockapi.io/bicycle');
     const data = await response.json();
     dispatch({ type: FETCH_BIKES_SUCCESS, payload: data });
   } catch (error) {
